fd847c3e103bcd773d41b991c813691192402862
Erick <erickmx@outlook.com>
2021-08-26 10:30:17 -0500

    Done ViewModel connection to View

---
+++ lands/Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,32 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Lands.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsRemembered { get; set; }
+
+        //COMANDS
+
+        public ICommand LoginCommand { get => new RelayCommand(LoginTap); }
+
+        private void LoginTap()
+        {
+            
+        }
+
+        public LoginViewModel()
+        {
+            IsRemembered = true;
+
+        }
+
+    }
+}
--- lands/Lands/Lands/ViewModels/MainViewModel.cs
+++ lands/Lands/Lands/ViewModels/MainViewModel.cs
@@ -6,6 +6,11 @@ namespace Lands.ViewModels
 {
     public class MainViewModel
     {
+        public LoginViewModel Login { get; set; }
 
+        public MainViewModel()
+        {
+            Login = new LoginViewModel();
+        }
     }
 }

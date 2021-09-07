8eeaa4f91210daa914176ce8be2c24161169cb3e
Erick <erickmx@outlook.com>
2021-09-07 17:14:49 -0500

    Lands List display with SearchBar

--- lands/Lands/Lands/ViewModels/LandsViewModel.cs
+++ lands/Lands/Lands/ViewModels/LandsViewModel.cs
@@ -1,23 +1,62 @@
-﻿using Lands.Models;
+﻿using GalaSoft.MvvmLight.Command;
+using Lands.Models;
 using Lands.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace Lands.ViewModels
 {
     public class LandsViewModel: BaseViewModel
     {
-        private APIServices apiService; 
+        #region Services
+        private APIServices apiService;
+        #endregion
 
+        #region Fields
         private ObservableCollection<Land> lands;
-        public ObservableCollection<Land> Lands 
+        private bool isRefreshing;
+        private string filter;
+        private List<Land> ogList;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<Land> Lands
         {
             get => lands;
             set => SetValue(ref lands, value);
         }
+        public bool IsRefreshing 
+        { 
+            get => isRefreshing;
+            set => SetValue(ref isRefreshing, value);
+        }
+        public string Filter
+        {
+            get => filter;
+            set
+            {
+                SetValue(ref filter, value);
+                Search();
+            }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand RefreshCommand 
+        {
+            get => new RelayCommand(LoadLands);
+        }
+        public ICommand SearchCommand
+        {
+            get => new RelayCommand(Search);
+        }
+
+        #endregion
 
         public LandsViewModel()
         {
@@ -25,12 +64,29 @@ namespace Lands.ViewModels
             LoadLands();
         }
 
+        private void Search()
+        {
+            if (string.IsNullOrEmpty(Filter))
+            {
+                Lands = new ObservableCollection<Land>(ogList);
+            }
+            else
+            {
+                Lands = new ObservableCollection<Land>(
+                    ogList.Where(
+                        lnd => lnd.Name.ToLower().Contains(Filter.ToLower()) ||
+                            lnd.Capital.ToLower().Contains(Filter.ToLower())));
+            }
+        }
+
         private async void LoadLands()
         {
+            IsRefreshing = true;
             var connection = await apiService.CheckConnection();
 
             if (!connection.isSuccess)
             {
+                IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert("Error", connection.Message, "Accept");
                 await Application.Current.MainPage.Navigation.PopAsync();
                 return;
@@ -40,8 +96,9 @@ namespace Lands.ViewModels
             if (!response.isSuccess)
                 await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
 
-            List<Land> list = (List<Land>) response.Result;
-            Lands = new ObservableCollection<Land>(list);
+            ogList = (List<Land>) response.Result;
+            Lands = new ObservableCollection<Land>(ogList);
+            IsRefreshing = false;
         }
     }
 }

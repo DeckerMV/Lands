d655c962b928fc6dccb5dd66e0898534330c0e9d
Erick <erickmx@outlook.com>
2021-09-15 11:54:49 -0500

    SingleTabbedPage finished

--- lands/Lands/Lands/ViewModels/SingleLandViewModel.cs
+++ lands/Lands/Lands/ViewModels/SingleLandViewModel.cs
@@ -10,19 +10,34 @@ namespace Lands.ViewModels
     public class SingleLandViewModel : BaseViewModel
     {
         private ObservableCollection<Border> borders;
+        private ObservableCollection<Currency> currencies;
+        private ObservableCollection<Language> languages;
 
         public Land SelectedLand { get; set; }
 
-        public ObservableCollection<Border> Borders 
+        public ObservableCollection<Language> Languages
+        {
+            get => languages;
+            set => SetValue(ref languages, value);
+        }
+
+        public ObservableCollection<Border> Borders
         {
             get => borders;
             set => SetValue(ref borders, value);
+        }
+        public ObservableCollection<Currency> Currencies
+        {
+            get => currencies;
+            set => SetValue(ref currencies, value);
         }
 
         public SingleLandViewModel(Land selectedLand)
         {
             SelectedLand = selectedLand;
             LoadBorders();
+            Currencies = new ObservableCollection<Currency>(SelectedLand.Currencies);
+            Languages = new ObservableCollection<Language>(SelectedLand.Languages);
         }
 
         private void LoadBorders()
